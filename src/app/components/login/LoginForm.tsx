9e5a1c2c04bb5e0f3d059047acde09b9a12e2d29
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-12-17 00:51:14 -0300

    feat: login page

---
+++ src/app/components/login/LoginForm.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "O E-mail é obrigatório." }),
+  password: z
+    .string({ message: "A senha é obrigatória." })
+    .min(6, { message: "A senha precisa ter ao menos 6 letras." }),
+  remember: z.boolean().default(false).optional(),
+});
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-y-4 font-jost w-full"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary font-normal">
+                Endereço de E-mail
+              </FormLabel>
+              <FormControl>
+                <Input
+                  className="h-12 text-base"
+                  placeholder="exemplo@email.com.br"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-primary font-normal">Senha</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  className="h-12 text-base"
+                  placeholder="Digite sua senha"
+                  {...field}
+                />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-between">
+          <FormField
+            control={form.control}
+            name="remember"
+            render={({ field }) => (
+              <FormItem className="flex items-center gap-x-2">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <div className="!mt-0 leading-none">
+                  <FormLabel className="font-normal">Lembre-me</FormLabel>
+                </div>
+              </FormItem>
+            )}
+          />
+          <Link
+            className="text-sm hover:text-muted-foreground"
+            href="/esqueci-minha-senha"
+          >
+            Esqueceu sua senha?
+          </Link>
+        </div>
+        <Button className="h-12" type="submit">
+          Entrar
+        </Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Image from "next/image";
+import LoginImg from "../../../public/login-bg.jpg";
+import ZenithLogo from "../../../public/zenith-logo-one.png";
+import { LoginForm } from "../components/login/LoginForm";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  return (
+    <main className="h-[100vh] flex justify-center flex-1 relative">
+      <button
+        className="absolute border border-primary p-2 rounded-md right-4 top-4"
+        onClick={router.back}
+      >
+        <ArrowLeft className="h-4 w-4" />
+      </button>
+      <div className="h-full w-full relative">
+        <Image
+          src={ZenithLogo}
+          alt="Zenith Logo"
+          width={160}
+          height={78}
+          className="absolute top-10 left-28 z-10 max-xl:left-16 max-lg:left-5 max-lg:top-3"
+        />
+        <Image
+          src={LoginImg}
+          alt="Imagem Página de Login"
+          fill
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="w-full flex flex-col justify-center items-start pl-28 pr-10 max-xl:pl-20 max-lg:pl-10">
+        <div className="max-w-[445px] w-full">
+          <h1 className="text-primary font-bold text-3xl font-poppins mb-1">
+            Bem-vindo(a)! 👋
+          </h1>
+          <p className="text-gray-400 font-jost text-base mb-7">
+            Faça seu login aqui
+          </p>
+          <LoginForm />
+          <div className="w-full mt-16">
+            <p className="text-gray-400 font-jost text-base mb-3 text-center">
+              Ainda não possui conta?
+            </p>
+            <Link
+              className="flex w-full border p-4 rounded-md justify-center font-jost font-bold text-sm hover:bg-gray-200"
+              href="/registrar"
+            >
+              Cadastrar
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
